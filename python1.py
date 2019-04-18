688ec14559afe8c2f40e39361a3bd8ec0bac989e
VGogol <luowenguo123@163.com>
2019-04-18 19:42:46 +0800

    use strip to delete space

--- python1.py
+++ python1.py
@@ -1,4 +1,6 @@
-message="hello python world!"
+message="   hello python world!    "
+print(message.strip())          #to delete space before and after the string
 print(message.title())
 print(message.upper())
 print(message.lower())
+
