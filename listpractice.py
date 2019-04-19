d106c15177a2866624295c7daf12c2d280263530
VGogol <luowenguo123@163.com>
2019-04-19 19:33:16 +0800

    book practice 3.2

---
+++ listpractice.py
@@ -0,0 +1,10 @@
+#coding=utf-8
+user1=['luo','wen','guo']
+#从列表删除不能来的，并输出
+notarrive=user1.pop(1)
+print(notarrive+" can't arrive")
+user1.insert(1,'hai')
+print(user1)
+user1.append('you')
+print(user1)
+
