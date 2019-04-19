93449d0f6bb368e702fa3f227ec9fe4cb10b6dfb
VGogol <luowenguo123@163.com>
2019-04-19 16:23:49 +0800

    use list

--- python1.py
+++ python1.py
@@ -3,4 +3,6 @@ print(message.strip())          #to delete space before and after the string
 print(message.title())
 print(message.upper())
 print(message.lower())
+go=['g','o','g','o','l']
+print(go)
 
